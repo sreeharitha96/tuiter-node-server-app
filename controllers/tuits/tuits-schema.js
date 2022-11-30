8153b7e9a5f0ef84c32f3a87bda3384696693b1a
sreeharitha1396 <vempati.sr@northeastern.edu>
2022-11-29 23:38:50 -0500

    dislike feature added

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 const schema = mongoose.Schema({
   tuit: String,
   likes: Number,
+  dislikes: Number,
   liked: Boolean,
 }, {collection: 'tuits'});
 export default schema;
